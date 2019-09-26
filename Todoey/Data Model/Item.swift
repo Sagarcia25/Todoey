70be4359219e8cd14ba3d98b681f9fa77ead65b5
Salvador <Sagarcia2713@gmail.com>
2019-09-25 21:36:48 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Salvador Garcia on 9/25/19.
+//  Copyright © 2019 Salvador Garcia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
